1a218a884c37d515136c7f3ccb8fb5112015ef82
JoshDuplisea <JoshuaS.Duplisea@gmail.com>
2018-05-22 22:38:19 -0300

    Update parse.php

--- parse.php
+++ parse.php
@@ -1,19 +1,19 @@
 <html>
-<head>
-  <title>File Splitter :: Results</title>
- </head>
- <body>
- <div style="margin:0 auto;width:980px;">
- <header>
-	<h1 style="text-align:center;">Export DATA</h1>
-	<span>Keep this tab open while you migrate the data:</span>
-	<ul>
-	<li>You can navigate to the tools/exporter/temp directory to mass download these copies</li>
-	<li>Go to the web directory for the new website, and place these files under the section you are copying over</li>
+  <head>
+    <title>File Splitter :: Results</title>
+  </head>
+  <body>
+    <div style="margin:0 auto;width:980px;">
+      <header>
+        <h1 style="text-align:center;">Export DATA</h1>
+        <span>Keep this tab open while you migrate the data:</span>
+        <ul>
+          <li>You can navigate to the tools/exporter/temp directory to mass download these copies</li>
+          <li>Go to the web directory for the new website, and place these files under the section you are copying over</li>
 	</ul>
-	</header>
-	<section>
- <?php
+      </header>
+      <section>
+<?php
 $ROOT = dirname(__FILE__);
 $target_path = "/uploads/";
 $target_path = $ROOT . $target_path . basename( $_FILES['uploadedfile']['name']); 
@@ -35,7 +35,7 @@ foreach ($newfiles as $value) {
 			$slug = explode("'", $slug);
 			$slug = $slug[1];
 			$slug = trim($slug);
-			echo "<a target='_blank' href='http://tools.adnetdev.com/exporter/temp/{$slug}'>" . $slug . "</a><br/>";
+			echo "<a target='_blank' href='[[website]]/exporter/temp/{$slug}'>" . $slug . "</a><br/>";
 			$handle = fopen($ROOT . "/temp/" . $slug, 'w+') or die("Unable to open file!");
 			file_put_contents("temp/" . $slug,$value);
 			fclose($handle);
@@ -82,10 +82,7 @@ foreach ($newfiles as $value) {
     //unlink($file);
 //}
 ?>
-</div>
-</section>
-<footer>
-<small>Please note: This tech is in alpha production. If you have any issues/questions, please email the creator at: <a href="mailto:jduplisea@adnetinc.com">jduplisea@adnetinc.com</a></small>
-</footer>
-<body>
+      </section>
+    </div>
+  <body>
 </html>
